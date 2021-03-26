2fb0b8a5756879f65553c12c4179963a8de3f5c2
Chunk-xo <up697529@myport.ac.uk>
2021-03-26 17:03:14 +0000

    3 files modified

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var useremail = user.email;
+    console.log(useremail);
+    $("#hello").append("<p>Hello", useremail, "</p>");
+    $('#signoutNav').show();
+    $('#accountNav').hide();
+  
+  } else {
+    // User is signed out
+    $('#signoutNav').hide();
+    $('#accountNav').show();
+  }
+});
+
 
 function createUser(){
   firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -44,6 +61,16 @@ function createNewAccount(){
       // Signed in 
       var user = userCredential.user;
       // ...
+      function sendEmailVerification() {
+        // [START auth_send_email_verification]
+        firebase.auth().currentUser.sendEmailVerification()
+          .then(() => {
+            // Email verification sent!
+            // ...
+          });
+        // [END auth_send_email_verification]
+      }
+      // ...
     })
     .catch((error) => {
       var errorCode = error.code;
@@ -52,7 +79,55 @@ function createNewAccount(){
     });
 }
 
+function loginAccount(){
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  // [END auth_signin_password]
+}
 
+function sendPasswordReset() {
+  const email = document.getElementById('email').value;
+  // [START auth_send_password_reset]
+  firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+      // Password reset email sent!
+      // ..
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+  // [END auth_send_password_reset]
+}
+
+function signout(){
+  firebase.auth().signOut().then(function() {
+    console.log('Signed Out');
+  }, function(error) {
+    console.error('Sign Out Error', error);
+  });
+}
+
+
+function showLoginBox(){
+  $(".fullscreen-container").fadeTo(200, 1);
+  //doesn't work
+  $('body').css("overflow", "hidden");
+  $("#closeButton").click(function() {
+    $(".fullscreen-container").fadeOut(200);
+  });
+}
 
 //to do: 
 //hide body scroll
