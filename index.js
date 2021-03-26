a87e18fc1322e744df660c9e82bfd44ab08ffd44
Chunk-xo <up697529@myport.ac.uk>
2021-03-26 13:20:35 +0000

    3 files modified

--- index.js
+++ index.js
@@ -33,37 +33,45 @@ function createUser(){
     });
 }
 
+function createNewAccount(){
+  //var firstname = document.getElementById('first_name').value;
+  //var lastname = document.getElementById('last_name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
 
-function writeUserData(userId, name, email) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}
+
+
+
+//to do: 
+//hide body scroll
+//body click closes modal
+$(function() {
+  $("#registerButton").click(function() {
+    $(".fullscreen-container").fadeTo(200, 1);
+    //doesn't work
+    $('body').css("overflow", "hidden");
   });
-}
+  $("#closeButton").click(function() {
+    $(".fullscreen-container").fadeOut(200);
+  });
+});
 
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//doesn't work
+$(window).click(function() {
+//Hide the menus if visible
+});
+$('.fullscreen-container').click(function(event){
+    event.stopPropagation();
+});
